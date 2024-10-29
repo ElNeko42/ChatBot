ac7114e5cdfcc8c47befd0ca02ba901f18e4149e
elneko42 <franson177@gmail.com>
2024-10-29 23:45:06 +0100

    Mostrar mensajes del chat en la tabla

--- resources/js/Pages/Chat/ChatIndex.vue
+++ resources/js/Pages/Chat/ChatIndex.vue
@@ -5,13 +5,20 @@
                 <template v-for="message in messages" :key="message.id">
                     <li class="px-4 py-2 my-2 flex justify-between font-semibold text-slate-400 bg-slate-900 hover:bg-slate-700 rounded-lg duration-200">
                         <link :href="`/chat/${message.id}`">{{message.context[0].content}}</link>
-
                     </li>
                 </template>
             </ul>
         </template>
             <div class="w-full flex text-white">
-
+                <template v-if="chat">
+                    <div class="w-full flex min-h-screen bg-slate-900">
+                        <div class="w-full overflow-auto">
+                            <template v-for="(content,index) in chat?.context" :key="index">
+                                <ChatContent :content="content" />
+                            </template>
+                        </div>
+                    </div>
+                </template>
             </div>
         <template #form>
             <section class="px-6 top-0">
@@ -38,7 +45,8 @@
 </template>
 <script setup>
 import ChatLayaout from '@/Layouts/ChatLayaout.vue';
-import { useForm } from '@inertiajs/vue3';
+import ChatContent from '@/Components/ChatContent.vue';
+import { useForm, Link } from '@inertiajs/vue3';
 const props = defineProps({
     messages: Array,
     chat: null | Object,
