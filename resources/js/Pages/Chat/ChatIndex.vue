a8eb60a6732890a42eafa02c2d4f2e2dca27617a
elneko42 <franson177@gmail.com>
2024-10-29 19:22:03 +0100

    cuadro de texto del chat

---
+++ resources/js/Pages/Chat/ChatIndex.vue
@@ -0,0 +1,16 @@
+<template>
+    <ChatLayaout>
+        <template #aside>
+
+        </template>
+            <div class="w-full flex text-white">
+
+            </div>
+        <template #form>
+        </template>
+
+    </ChatLayaout>
+</template>
+<script setup>
+import ChatLayaout from '@/Layouts/ChatLayaout.vue';
+</script>
