f0d72218e2e431f681182e3df57d291ef8b5cea5
elneko42 <franson177@gmail.com>
2024-10-29 20:39:02 +0100

    Añadir input text

--- resources/js/Pages/Chat/ChatIndex.vue
+++ resources/js/Pages/Chat/ChatIndex.vue
@@ -7,6 +7,13 @@
 
             </div>
         <template #form>
+            <section class="px-6 top-0">
+                <div class="w-full">
+                    <div class="relative flex-1 flex items-center">
+                        <input type="text" class="w-full bg-slate-700 text-white rounded-lg" placeholder="Envía un mensaje al ChatBot ... ">
+                    </div>
+                </div>
+            </section>
         </template>
 
     </ChatLayaout>
